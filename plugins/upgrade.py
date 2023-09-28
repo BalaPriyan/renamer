2ac3a6674a3119903487636146e25ee60f00a905
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2023-09-28 10:36:05 -0700

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -20,15 +20,14 @@ async def upgrade(bot,update):
 	Daily Upload limit 100GB
 	Price Rs 206  ind /🌎 2.5$  per Month
 	
-	
-	Pay Using Upi I'd ```7808912076@paytm```
+
 	
 	After Payment Send Screenshots Of 
-        Payment To Admin @mRiderDM"""
+        Payment To Admin @NoMoreFiles"""
 	keybord = InlineKeyboardMarkup([[ 
-        			InlineKeyboardButton("ADMIN 🛂",url = "https://t.me/mRiderDM")], 
-        			[InlineKeyboardButton("Paytm",url = "https://p.paytm.me/xCTH/vo37hii9"),
-        			InlineKeyboardButton("Paytm",url = "https://p.paytm.me/xCTH/vo37hii9")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+        			InlineKeyboardButton("ADMIN 🛂",url = "https://t.me/BalaPriyanB")], 
+        			[InlineKeyboardButton("Buy Me Coffee",url = "https://www.buymeacoffee.com/kingoffondness"),
+        			InlineKeyboardButton("Buy Me Coffee",url = "https://www.buymeacoffee.com/kingoffondness")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
 	await update.message.edit(text = text,reply_markup = keybord)
 	
 
@@ -50,13 +49,12 @@ async def upgradecm(bot,message):
 	Daily Upload limit 100GB
 	Price Rs 206  ind /🌎 2.5$  per Month
 	
-	
-	Pay Using Upi I'd ```7808912076@paytm```
+
 	
 	After Payment Send Screenshots Of 
-        Payment To Admin @mRiderDM"""
+        Payment To Admin @BalaPriyanB"""
 	keybord = InlineKeyboardMarkup([[ 
-        			InlineKeyboardButton("ADMIN 🛂",url = "https://t.me/mRiDerDM")], 
-        			[InlineKeyboardButton("Paytm",url = "https://p.paytm.me/xCTH/vo37hii9"),
-        			InlineKeyboardButton("Paytm",url = "https://p.paytm.me/xCTH/vo37hii9")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+        			InlineKeyboardButton("ADMIN 🛂",url = "https://t.me/BalaPriyanB")], 
+        			[InlineKeyboardButton("Paytm",url = "https://www.buymeacoffee.com/kingoffondness"),
+        			InlineKeyboardButton("Pay",url = "https://www.buymeacoffee.com/kingoffondness")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
 	await message.reply_text(text = text,reply_markup = keybord)
